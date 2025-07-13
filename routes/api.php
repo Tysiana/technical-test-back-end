b9ece6f53045fd9f228ece6b9a1a70356baeadb2
Tysiana <calajustyna@gmail.com>
2025-07-13 17:08:20 +0100

    api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Resources\ComponentCollection;
+use App\Http\Resources\InspectionCollection;
+use App\Http\Resources\ComponentTypeCollection;
+use App\Http\Resources\FarmResource;
+use App\Http\Resources\TurbineCollection;
+use App\Http\Resources\TurbineResource;
+use App\Http\Resources\ComponentResource;
+use App\Http\Resources\InspectionResource;
+use App\Http\Resources\GradeCollection;
+use App\Http\Resources\GradeTypeCollection;
+use App\Http\Resources\GradeResource;
+use App\Models\Grade;
+use App\Models\Component;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Farm;
+use App\Models\Turbine;
+use App\Models\ComponentType;
+use App\Models\GradeType;
+use App\Models\Inspection;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +31,87 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Farms
+Route::get('farms', function () {
+     return FarmResource::collection(Farm::all());
+});
+Route::get('farms/{farm}', function (Farm $farm) {
+    return new FarmResource($farm);
+});
+Route::get('farms/{farm}/turbines', function (Farm $farm) {
+     return new TurbineCollection($farm->turbines);
+});
+Route::get('farms/{farm}/turbines/{turbine}', function (Farm $farm, Turbine $turbine) {
+    return new TurbineResource($turbine);
+});
+
+// Turbines
+Route::get('turbines', function () {
+     return TurbineResource::collection(Turbine::all());
+});
+Route::get('turbines/{turbine}', function (Turbine $turbine) {
+     return new TurbineResource($turbine);
+});
+Route::get('turbines/{turbine}/components', function (Turbine $turbine) {
+     return new ComponentCollection($turbine->components);
+});
+Route::get('turbines/{turbine}/components/{component}', 'App\\Http\\Controllers\\ComponentController@show');
+Route::get('turbines/{turbine}/inspections', function (Turbine $turbine) {
+     return new InspectionCollection($turbine->inspections);
+});
+Route::get('turbines/{turbine}/inspections/{inspection}', function (Turbine $turbine, Inspection $inspection) {
+    return new InspectionResource($inspection);
+});
+
+// Components
+Route::get('components', function () {
+     return new ComponentCollection(Component::all());
+});
+Route::get('components/{component}', function (Component $component) {
+    return new ComponentResource($component);
+});
+
+Route::get('components/{component}/grades', function (Component $component) {
+     return new GradeCollection($component->grades);
+});
+
+Route::get('components/{component}/grades/{grade}', function (Component $component, Grade $grade) {
+    return new GradeResource($grade);
+});
+
+// Inspections
+Route::get('inspections', function () {
+     return new InspectionCollection(Inspection::all());
+});
+
+Route::get('inspections/{inspection}', function (Inspection $inspection) {
+    return new InspectionResource($inspection);
+});
+
+Route::get('inspections/{inspection}/grades', function (Inspection $inspection) {
+     return new GradeCollection($inspection->grades);
+});
+
+Route::get('inspections/{inspection}/grades/{grade}', function (Inspection $inspection, Grade $grade) {
+    return new GradeResource($grade);
+});
+
+Route::get('grades/{grade}', function (Grade $grade) {
+    return new GradeResource($grade);
+});
+
+// Component Types
+Route::get('component-types', function () {
+     return new ComponentTypeCollection(ComponentType::all());
+});
+Route::get('component-types/{componentType}', function (ComponentType $componentType) {
+    return new ComponentResource($componentType);
+}); 
+
+// Grade Types
+Route::get('grade-types', function () {
+     return new GradeTypeCollection(GradeType::all());
+});
+Route::get('grade-types/{gradeType}', function (GradeType $gradeType) {
+    return new GradeResource($gradeType);
+});
